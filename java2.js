4441e546bbb780946fdf500a2283c97904c0ba23
coton12 <143169100+coton12@users.noreply.github.com>
2026-05-30 02:03:16 -0600

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,16 @@
+
+
+
+    // Lógica para resolver la multiplicación
+    function calcularMultiplicacion() {
+            const n1 = parseFloat(document.getElementById('multNum1').value);
+    const n2 = parseFloat(document.getElementById('multNum2').value);
+    const rContainer = document.getElementById('resMult');
+
+    if (isNaN(n1) || isNaN(n2)) {
+        rContainer.textContent = "Por favor, llena ambos campos.";
+    return;
+            }
+
+    rContainer.textContent = `Resultado: ${n1 * n2}`;
+        }
